ca32563be296b0359f631192e4dda8a04d262917
Shahriar075 <shahriarjoyy@gmail.com>
2023-05-20 00:16:32 +0600

    framework_concept

---
+++ src/main/java/org/example/Bike.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Bike implements Vehicle{
+    @Override
+    public void drive() {
+        System.out.println("The bike is running");
+    }
+}
